45920d0b53b9daaf70d1b89ce5628e4ccacaa95a
ms919 <81754336+ms919@users.noreply.github.com>
2023-01-08 06:26:59 +0000

    一連機能作成

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,19 +1,38 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: %i(edit update destroy)
+  
   def index
+    @tasks = Task.all
   end
 
   def new
+    @task = Task.new
   end
 
   def create
+    @task = Task.new(task_params)
+    @task.save
   end
 
   def edit
   end
 
   def update
+    @task.update(task_params)
   end
 
   def destroy
+    @task.delete
+    redirect_to tasks_path
+  end
+
+  private
+
+  def set_task
+    @task = Task.find(params[:id])
+  end
+
+  def task_params
+    params.require(:task).permit(:title, :detail, :status)
   end
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,3 @@
 class Task < ApplicationRecord
+  enum status: { untouched: 0, launch: 1, done: 2, pending: 3, withdraw: 4 }
 end
